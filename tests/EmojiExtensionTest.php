5dff1a455d797c8facf726d6092e384aa85c4cf9
Erik Giberti <erik@af-design.com>
2022-02-13 16:49:07 -0600

    Newlines are preserved, not converted to breaks

--- tests/EmojiExtensionTest.php
+++ tests/EmojiExtensionTest.php
@@ -73,7 +73,7 @@ class EmojiExtensionTest extends TestCase
             ],
             'text is not swallowed when using colon as punctuation' => [
                 "line: 1\nlost line: 2\nother text\nthen :hot_beverage:",
-                "<p>line: 1<br />lost line: 2<br />other text<br />then " . '<span class="emoji" title="hot_beverage">☕</span></p>',
+                "<p>line: 1\nlost line: 2\nother text\nthen " . '<span class="emoji" title="hot_beverage">☕</span></p>',
             ]
         ];
     }
